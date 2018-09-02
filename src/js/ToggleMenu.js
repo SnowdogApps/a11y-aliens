6957fe5052e3633810cdd2f23ad6290dde446cc2
Ania Karon <anna.karon@snow.dog>
2018-09-02 19:27:22 +0200

    fix keyboard support

--- src/js/ToggleMenu.js
+++ src/js/ToggleMenu.js
@@ -8,15 +8,47 @@ export default class {
   }
   init () {
     this.menuTrigger.addEventListener('click',
-      () => this.toggle(this.navMenu)
+      () => this.toggle()
     )
+    window.addEventListener('resize', () => {
+      this.setNavVisibility();
+    });
+    window.addEventListener('keydown', (e) => {
+      if (e.which === 27
+        && this.navMenu.classList.contains(this.openClass)
+      ) {
+        this.closeMenu();
+        const mainContent = document.querySelector('.main'),
+              target = mainContent.querySelectorAll('h1, h2, h3, h4, h5, h6')[0];
+        target.setAttribute('tabindex', '-1');
+        target.focus();
+      }
+    });
   }
-  toggle(navMenu) {
-    if (navMenu.classList.contains(this.openClass)) {
-      navMenu.classList.remove(this.openClass);
+  toggle () {
+    if (this.navMenu.classList.contains(this.openClass)) {
+      this.menuTrigger.setAttribute('aria-expanded', false);
+      this.navMenu.setAttribute('aria-hidden', true);
+      this.closeMenu();
     }
     else {
-      navMenu.classList.add(this.openClass);
+      this.menuTrigger.setAttribute('aria-expanded', true);
+      this.navMenu.classList.add(this.openClass);
+      this.navMenu.setAttribute('aria-hidden', false);
+      this.navMenu.querySelectorAll('a[href]')[0].focus();
+    }
+  }
+  closeMenu () {
+    this.navMenu.classList.remove(this.openClass);
+  }
+  setNavVisibility () {
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      this.navMenu.setAttribute('aria-hidden', false);
+    }
+    else {
+      if (!this.navMenu.classList.contains(this.openClass)) {
+        this.navMenu.setAttribute('aria-hidden', true);
+      }
     }
   }
 }
